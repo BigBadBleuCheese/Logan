c4aa85032fdf23273c4ab977abb76a3e80280db2
Daniel Henry <daniel.henry@epiforge.com>
2022-07-27 23:15:59 -0400

    even better

--- Logan/Logan.lua
+++ Logan/Logan.lua
@@ -63,7 +63,7 @@ LoganFrame:SetScript('OnEvent', function(self, event, ...)
 	if Settings.Enabled then
 		if event == 'CHAT_MSG_CHANNEL' then
 			local text, _, _, channelName = ...
-			if text:find([[^Never thought I'd find myself back in .*? again\.$]]) then
+			if text:find("Never thought I'd find myself back in .+ again\.") then
 				SendChatMessage("That's what I'm say'n!", channelName)
 			end
 		elseif event == 'ZONE_CHANGED_NEW_AREA' then
